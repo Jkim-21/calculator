5b4bccf0435e0e74284b420b54b69ee6525f05a9
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-12-03 23:10:50 -0500

    Add number and operation functionality
    Number and operations buttons are now responsive to clicks,
    displaying the current operand along with displaying the past
    numbers and operations in the display above.

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
-let firstNumber = 
-let secondNumber = 
-let result = 
+const numberButtons = document.querySelectorAll(".number");
+const deleteButton = document.querySelector(".delete");
+const operationButtons = document.querySelectorAll(".operation");
+const clearButton = document.querySelector(".clear");
+const equalsButton = document.querySelector(".equals");
+const previousOperandDisplay = document.querySelector(".previous-operand");
+const currentOperandDisplay = document.querySelector(".current-operand");
 
+let currentOperand = "";
+let previousOperand = "";
+let currentOperation = undefined;
+
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener("click", () => {
+        appendNumber(numberButton.textContent);
+    });
+});
+
+operationButtons.forEach(operationButton => {
+    operationButton.addEventListener("click", () => {
+        selectOperation(operationButton.textContent);
+    });
+});
+
+
+function appendNumber(number) {
+    if (currentOperandDisplay.textContent === "0") {
+        currentOperand = "";
+    }
+    currentOperand += number;
+    currentOperandDisplay.textContent = currentOperand;
+}
+
+function selectOperation(operation) {
+    if (currentOperand !== "" && currentOperation === undefined) {
+        previousOperand = currentOperand;
+        currentOperation = operation;
+
+        previousOperandDisplay.textContent += `${currentOperand} ${operation}`;
+
+        currentOperand = "";
+        compute();
+    }
+}
+
+function compute() {
+    if (currentOperand === undefined) {
+        return;
+    }
+
+}
 
 function add(num1, num2) {
     return num1 + num2;
